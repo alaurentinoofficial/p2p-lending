7ba95b85f74b80a1a299bdfcba50a1e37e796ec5
Anderson Laurentino <alaurentino.br@gmail.com>
2019-03-31 16:48:32 -0300

    Create a first Transferer test

---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"p2p-lending/models"
+	"time"
+)
+
+func main() {
+	// ----------------------------------------
+	user1 := models.User{
+		Name: "Anderson Laurentino",
+		Email: "alaurentino.br@gmail.com",
+		Password: "1234567890n",
+		CpfCnpj: "12345678901",
+		Score: 967,
+		Balance: float32(0),
+	}
+	user1.Create()
+
+	user2 := models.User{
+		Name: "João Matheus",
+		Email: "jmgc@gmail.com",
+		Password: "1234567890n",
+		CpfCnpj: "12345678901",
+		Score: 870,
+		Balance: float32(30000),
+	}
+	user2.Create()
+
+	user3 := models.User{
+		Name: "Helton Alves",
+		Email: "jhap@gmail.com",
+		Password: "1234567890n",
+		CpfCnpj: "12345678901",
+		Score: 680,
+		Balance: float32(30000),
+	}
+	user3.Create()
+	// ----------------------------------------
+
+
+	// ----------------------------------------
+	lending := models.Lending{
+		Taker: user1.ID,
+		AlreadyInvested: 0,
+		Amount: float32(10000),
+		HasIndexer: false,
+		Indexer: "NONE",
+		Yield: float32(2.5),
+		Validate: time.Now().AddDate(0,1, 0).String(),
+		Status: false,
+	}
+	lending.Create()
+	// ----------------------------------------
+
+	// ----------------------------------------
+	lender1 := models.Lender{
+		User: user2.ID,
+		Amount: float32(3000),
+		Status: false,
+		Lending: lending.ID,
+	}
+	lender1.Create()
+
+	lender2 := models.Lender{
+		User: user3.ID,
+		Amount: float32(7000),
+		Status: false,
+		Lending: lending.ID,
+	}
+	lender2.Create()
+
+	lending.AlreadyInvested += lender1.Amount + lender2.Amount
+	lending.Save()
+	// ----------------------------------------
+
+	if lending.Amount == lending.AlreadyInvested {
+		fmt.Println("[*] Transfering...")
+		lending.Transfer()
+	}
+
+	// ----------------------------------------
+	fmt.Println("Lending Status: ", lending.Status)
+
+	fmt.Println("User 1 Balance: ", models.GetUserById(user1.ID).Balance)
+
+	fmt.Println("User 2 Balance: ", models.GetUserById(user2.ID).Balance)
+
+	fmt.Println("User 3 Balance: ", models.GetUserById(user3.ID).Balance)
+}
